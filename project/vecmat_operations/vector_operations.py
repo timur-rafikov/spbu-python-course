37abc1e9418c0afe29faaa109c7a8f0131562b92
Timur Rafikov <st128186@student.spbu.ru>
2025-10-02 15:44:24 +0300

    Added a vector class with basic operations

---
+++ project/vecmat_operations/vector_operations.py
@@ -0,0 +1,83 @@
+from math import sqrt, acos
+
+
+class Vector:
+    def __init__(self, data: list[float]):
+        """
+        Initializes a Vector object
+
+        Args:
+            data (list[float]): A list of values to create the vector
+        """
+        self.vector = data
+
+    def __len__(self) -> int:
+        """
+        Returns the length of the vector
+
+        Returns:
+            int: The number of elements in the vector
+        """
+        return len(self.vector)
+
+    def __mul__(self, other: "Vector") -> float:
+        """
+        Computes the dot product of two vectors
+
+        Args:
+            other (Vector): Other vector
+
+        Raises:
+            ValueError: If the lengths of the vectors are not the same
+
+        Returns:
+            float: The dot product of the two vectors.
+        """
+        if len(self) != len(other):
+            raise ValueError("Vectors must have the same length")
+
+        return sum(self.vector[i] * other.vector[i] for i in range(len(self)))
+
+    def norm(self) -> float:
+        """
+        Computes the norm (length) of the vector.
+
+        Returns:
+            float: The norm of the vector
+        """
+        return sqrt(sum(x**2 for x in self.vector))
+
+    def __xor__(self, other: "Vector") -> float:
+        """
+        Computes the angle between two vectors
+
+        Args:
+            other (Vector): The vector to find the angle with
+
+        Raises:
+            ValueError:         If the lengths of the vectors are not the same
+            ZeroDivisionError:  If the norm of one of the vectors is zero.
+
+        Returns:
+            float: The angle between the two vectors in radians.
+        """
+        if len(self) != len(other):
+            raise ValueError("Vectors must have the same length")
+
+        self_norm = self.norm()
+        other_norm = other.norm()
+
+        if self_norm == 0 or other_norm == 0:
+            raise ZeroDivisionError("The norm of one of the vectors is zero")
+
+        dot_prod = self * other
+        return acos(dot_prod / (self_norm * other_norm))
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the vector.
+
+        Returns:
+            str: A string representation of the vector.
+        """
+        return f"Vector({self.vector})"
